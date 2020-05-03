9b111695e3e6e1a1d80eb0749602ca55921f1a5b
shangjian <shangjian2019@outlook.com>
2020-05-03 17:26:14 +0800

    Initial Commit

---
+++ pages/demo01/demo01.js
@@ -0,0 +1,37 @@
+// pages/demo01/demo01.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"你好，世界",
+    num:99999999,
+    isGirl:false,
+    prosn:{
+      name:"Hornet",
+      age : 33,
+      size : "男",
+      height:179
+    },
+    checkd:false,
+    list:[
+      {
+        id:0,
+        name:"八届"
+      },
+      {
+        id:1,
+        name:"无能为力"
+      },
+      {
+        id:2,
+        name:"天蓬元帅"
+      },
+      {
+        id:3,
+        name:"猪刚烈"
+      }
+    ]
+  }
+})
